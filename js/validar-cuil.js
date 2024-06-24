094f087fa7baa520a8291c91be84601e104ae437
AmyQuin <149535311+AmyQuin@users.noreply.github.com>
2024-06-24 01:43:33 -0600

    03. Desarrollando validaciones
    Lo que aprendimos en esta aula:
    
    -Valide los dígitos de control de Cuil/Cuit;
    -Transforme fórmulas matemáticas en código;
    -Tratar con fechas en JavaScript;
    -Construya comparaciones de fechas para verificar la edad.

--- js/validar-cuil.js
+++ js/validar-cuil.js
@@ -1,12 +1,19 @@
-export default function esUnCuil(campo){
-    const cuil = campo.value.replace(/[-\/]/g,"");
-    tieneNumerosRepetidos(cuil)
-    console.log(cuil);
-    console.log(tieneNumerosRepetidos(cuil));
+export default function esUnCuil(campo) {
+    const cuil = campo.value.replace(/[-\/]/g, "");
+
+    if (tieneNumerosRepetidos(cuil)) {
+        console.log("Valores repetidos");
+    } else {
+        if (validarPrimerosDigitos(cuil) && validarDigitosVerificador(cuil)) {
+            console.log("Cuil valido");
+        } else {
+            console.log("Cuil no existe");
+        }
+    }
 }
 
-function tieneNumerosRepetidos(cuil){
-    const numerosRepetidos=[
+function tieneNumerosRepetidos(cuil) {
+    const numerosRepetidos = [
         "00000000000",
         "11111111111",
         "33333333333",
@@ -19,4 +26,30 @@ function tieneNumerosRepetidos(cuil){
     ];
 
     return numerosRepetidos.includes(cuil);
+}
+
+function validarPrimerosDigitos(cuil) {
+    let primerosDigitos = cuil.substr(0, 2);
+    let digitosValidos = ['20', '23', '24', '27', '30', '33', '34'];
+    return digitosValidos.includes(primerosDigitos)
+}
+
+function validarDigitosVerificador(cuil) {
+    let acumulado = 0;
+    const factores = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
+
+    for (let i = 0; i < 10; i++) {
+        acumulado += parseInt(cuil[i], 10) * factores[i];
+    }
+    let validadorTeorico = 11 - (acumulado % 11); //nos devolvera el resto de una division
+
+    if (validadorTeorico == 11) {
+        validadorTeorico = 0
+    } else if (validadorTeorico == 10) {
+        validadorTeorico = 9
+    }
+
+    const digitoVerificador = parseInt(cuil[10], 10)
+
+    return digitoVerificador === validadorTeorico;
 }
